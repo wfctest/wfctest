dbf9a697e4bd0c8475598f71a77a7e9076223773
AndrewLester <alester220@gmail.com>
2019-10-24 17:58:52 -0400

    Update two files and change page text

--- js/header.js
+++ js/header.js
@@ -1,7 +1,7 @@
 navbar = document.getElementsByTagName('nav')[0];
 
 window.addEventListener('scroll', function(e) {
-    if (this.scrollY > 0 && this.innerWidth > 925) {
+    if (this.scrollY > 0 && this.innerWidth > 1050) {
         navbar.classList.add('floating');
     } else {
         navbar.classList.remove('floating');
