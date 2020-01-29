da393b89582e4b964edb2ff69362ec9a2afab199
AndrewLester <alester220@gmail.com>
2020-01-28 20:29:05 -0500

    Add live, serve, and learn pages

--- js/header.js
+++ js/header.js
@@ -1,4 +1,4 @@
-navbar = document.getElementsByTagName('nav')[0];
+var navbar = document.getElementsByTagName('nav')[0];
 
 window.addEventListener('scroll', function(e) {
     if (this.scrollY > 0 && this.innerWidth > 1050) {
