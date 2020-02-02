2a6f739d9a1ab88454539ee69e4f79b06aa34df6
AndrewLester <alester220@gmail.com>
2020-02-01 20:22:03 -0500

    Recreate mobile nav menu

--- js/header.js
+++ js/header.js
@@ -1,9 +1,19 @@
 var navbar = document.getElementsByTagName('nav')[0];
+var body = document.getElementsByTagName('body')[0];
+var navButton = document.getElementById('nav-list-button');
 
 window.addEventListener('scroll', function(e) {
-    if (this.scrollY > 0 && this.innerWidth > 1050) {
+    if (this.scrollY > 0 || this.innerWidth < 1050) {
         navbar.classList.add('floating');
     } else {
         navbar.classList.remove('floating');
     }
+});
+
+if (window.innerWidth < 1050) {
+    navbar.classList.add('floating');
+}
+
+navButton.addEventListener('click', function() {
+    body.classList.toggle('nav-open');
 });
