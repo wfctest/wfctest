1c0c527b157dba2d1dec443de986ee673a3dc64b
AndrewLester <alester220@gmail.com>
2020-09-08 14:50:04 -0400

    Revert relative links and add welcoming week banner

--- js/header.js
+++ js/header.js
@@ -1,6 +1,8 @@
 var navbar = document.getElementsByTagName('nav')[0];
 var body = document.getElementsByTagName('body')[0];
 var navButton = document.getElementById('nav-list-button');
+var bannerButton = document.getElementById('banner-button');
+var bannerInput = document.getElementById('banner-email');
 
 window.addEventListener('scroll', function(e) {
     if (this.scrollY > 0 || this.innerWidth < 1050) {
@@ -16,4 +18,15 @@ if (window.innerWidth < 1050) {
 
 navButton.addEventListener('click', function() {
     body.classList.toggle('nav-open');
+});
+
+bannerButton.addEventListener('click', function() {
+    var request = new XMLHttpRequest();
+    request.open('POST', '/unknown', true);
+    request.send(bannerInput.value);
+    bannerInput.value = '';
+    bannerButton.value = 'Sent';
+    setTimeout(function() { 
+        bannerButton.value = 'Sign up';
+    }, 2000);
 });
