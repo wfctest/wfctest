47bb0b57a4cf1ebecbdd10f29eecbf04f638e9d1
AndrewLester <alester220@gmail.com>
2020-09-27 22:12:48 -0400

    Remove unused nav class

--- js/header.js
+++ js/header.js
@@ -3,21 +3,16 @@ var body = document.getElementsByTagName('body')[0];
 var navButton = document.getElementById('nav-list-button');
 var bannerButton = document.getElementById('banner-button');
 var bannerInput = document.getElementById('banner-email');
-var FLOATING_HEADER_WIDTH = 1050;
 
 window.addEventListener('scroll', function(e) {
-    if (this.scrollY > 0) {
+    if (this.scrollY > 0 || this.innerWidth < 1050) {
         navbar.classList.add('floating');
-        navbar.classList.remove('scroll-top');
     } else {
-        if (window.innerWidth > FLOATING_HEADER_WIDTH) {
-            navbar.classList.remove('floating');
-        }
-        navbar.classList.add('scroll-top');
+        navbar.classList.remove('floating');
     }
 });
 
-if (window.innerWidth < FLOATING_HEADER_WIDTH) {
+if (window.innerWidth < 1050) {
     navbar.classList.add('floating');
 }
 
